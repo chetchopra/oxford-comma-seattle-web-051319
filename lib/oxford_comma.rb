70cc551c391bb32a395f880a77532a30731547c2
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 18:54:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(arr)
+  str = ""
   amt = arr.size() - 1
   amt.times do |i|
-    puts arr[i]
+    str += " #{arr[i]}, "
   end
+  return str
 end
 
 oxford_comma(["DHJK", "fjhd", "fhsd"])
