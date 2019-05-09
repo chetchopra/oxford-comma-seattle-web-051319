041ca12fcb6f7568552a74ba9450863a9285a170
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 18:58:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(arr)
   if arr.size() == 1
     return arr[0]
   end
+  if arr.size() == 2
+    return "#{arr[0]} and #{arr[1]}"
+  end
   str = ""
   amt = arr.size() - 1
   amt.times do |i|
